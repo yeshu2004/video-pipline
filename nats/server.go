8f7b012ded8f0f992337f01da0f6e2c39c066995
yeshu2004 <playeryeshu@gmail.com>
2026-02-02 19:34:11 +0530

    working & fix: correct video uploaded event publishing and improve error handling in NATS integration

--- nats/server.go
+++ nats/server.go
@@ -5,13 +5,23 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
+
+	// "path/filepath"
+
+	// "os/exec"
 	"strconv"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/jetstream"
+	"github.com/yeshu2004/video-pipline/aws"
 	"github.com/yeshu2004/video-pipline/models"
 )
 
@@ -49,7 +59,7 @@ func (nats *Nats) CreateVideoStream(ctx context.Context) error {
 }
 
 func (nats *Nats) PublishVideoUplodedEvent(ctx context.Context, key string, payload []byte) error {
-	_, err := nats.js.Publish(ctx, "VIDEO.uploaded", payload, jetstream.WithMsgID(fmt.Sprintf("video-%d", key)))
+	_, err := nats.js.Publish(ctx, "VIDEO.uploaded", payload, jetstream.WithMsgID(fmt.Sprintf("video-%s", key)))
 	return err
 }
 
@@ -87,6 +97,10 @@ func (nats *Nats) ConsumeFFmpeg480Event(ctx context.Context) error {
 		return err
 	}
 
+	godotenv.Load();
+	cfg := aws.LoadAwsConifg();
+	service := aws.NewS3Service(cfg);
+
 	for {
 		select {
 		case <-ctx.Done():
@@ -103,7 +117,7 @@ func (nats *Nats) ConsumeFFmpeg480Event(ctx context.Context) error {
 			}
 
 			for msg := range msgs.Messages() {
-				if err := processVideoForFFmpeg(ctx, "480", msg.Data()); err != nil {
+				if err := processVideoForFFmpeg(ctx, service, "480", msg.Data()); err != nil {
 					log.Println("480 processing failed:", err)
 					msg.Nak()
 					continue
@@ -115,7 +129,7 @@ func (nats *Nats) ConsumeFFmpeg480Event(ctx context.Context) error {
 	}
 }
 
-func processVideoForFFmpeg(ctx context.Context, videoQuality string, eventData []byte) error {
+func processVideoForFFmpeg(ctx context.Context, awsService *aws.S3Service, videoQuality string, eventData []byte) error {
 	quality, err := strconv.Atoi(videoQuality)
 	if err != nil {
 		return err
@@ -126,22 +140,60 @@ func processVideoForFFmpeg(ctx context.Context, videoQuality string, eventData [
 		return fmt.Errorf("json unmarshal error: %v", err)
 	}
 
-	log.Printf("processing video with key %s at %dp", payload.Key, quality)
+	log.Printf("processing video with key %s at %dp\n", payload.Key, quality)
 
-	filePath := ""
-	outputFilePath := ""
+	// 0. fetch the presigned url from s3 service
+	url, err := awsService.FetchGetPresignedURL(ctx, payload.Bucket, payload.Key);
+	if err != nil{
+		return  fmt.Errorf("error fetching sigend url of given file (%s) over bucket (%s): error: %v", payload.Key, payload.Bucket, err);
+	}
 
-	//1. download the video using http.get
-	//2. copy into raw folder
-	//3. run the ffmpeg command
-	//4. store in processed folder
-	//5. upload back to cloud
+	// 1. download the video using http.get
+	resp, err := http.Get(url);
+	if err != nil{
+		return err;
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download video: status %d", resp.StatusCode)
+	}
+	log.Printf("downloaded video with key %s from s3 bucket successfully\n", payload.Key);	
+	defer resp.Body.Close()
+
+	//2. create local directory and file
+	localPath := payload.Key;
+	if err := os.MkdirAll(filepath.Dir(localPath), 0755); err != nil {
+		return err
+	}
+	log.Printf("created local directory for key %s successfully\n", payload.Key);
+
+	out, err := os.Create(localPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	log.Printf("created local file for key %s successfully\n", payload.Key);
+
+	//3. copy the downloaded content into local file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Printf("saved video to local file for key %s successfully\n", payload.Key);
+
+	// //3. run the ffmpeg command
+	outputFolder:= "processed/480p"
+	if err := os.MkdirAll(outputFolder, 0755);err != nil{
+		return err;
+	}
+	log.Printf("created output directory %s successfully\n", outputFolder);
+
+	outputFilePath := filepath.Join(outputFolder, filepath.Base(payload.Key));
 
 	scale := fmt.Sprintf("scale=-2:%d", quality)
 	cmd := exec.CommandContext(
 		ctx,
 		"ffmpeg",
-		"-i", filePath,
+		"-i", localPath,
 		"-vf", scale,
 		"-c:v", "libx264",
 		"-crf", "23",
@@ -153,6 +205,10 @@ func processVideoForFFmpeg(ctx context.Context, videoQuality string, eventData [
 	if err != nil {
 		return fmt.Errorf("ffmpeg error: %v, output: %s", err, string(b))
 	}
+	// //4. store in processed folder
+	// //5. upload back to cloud
 
+
+	log.Printf("processing video done with key %s at %dp\n", payload.Key, quality)
 	return nil
 }
---
+++ worker/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/yeshu2004/video-pipline/nats"
+)
+
+func main() {
+	ctx := context.Background()
+
+	nat, err := nats.NewNATS()
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	if err := nat.CreateVideoStream(ctx); err != nil {
+		log.Fatalf("create video stream failed: %v", err)
+	}
+
+	if err := nat.CreateFFmpeg480Consumer(ctx); err != nil {
+		log.Fatalf("create ffmpeg 480 consumer failed: %v", err)
+	}
+
+	if err := nat.ConsumeFFmpeg480Event(ctx); err != nil {
+		log.Fatalf("consume ffmpeg 480 consumer failed: %v", err)
+	}
+
+}
